204625e3e85d98a7f16f5c98ec9fcc66a9e4e250
saxenaharsh <harsh.saxena@gmail.com>
2020-05-18 14:44:19 +0530

    Add files via upload

---
+++ DiagonalStar/src/com/HarshSaxena/Main.java
@@ -0,0 +1,27 @@
+package com.HarshSaxena;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        printSquareStar(10);
+
+    }
+    public static void printSquareStar(int number){
+        if(number < 5){
+            System.out.println("Invalid Value");
+            return;
+        }
+        for(int i = 1; i <= number; i++){
+            for(int j = 1; j <= number; j++){
+                if(i == 1 || j == 1 || i == number || j == number || i == j || j == number + 1 -i){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
